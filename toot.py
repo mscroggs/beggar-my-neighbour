e027d2741ebd8bb98f33de4f5455524b21f88bb5
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-04-02 12:27:09 +0100

    remove testing

--- toot.py
+++ toot.py
@@ -2,8 +2,6 @@ from mastodon import Mastodon
 
 
 def toot(text):
-    print(text)
-    return
     mdon = Mastodon(
         access_token="mastodon.secret", api_base_url="https://mathstodon.xyz")
 
